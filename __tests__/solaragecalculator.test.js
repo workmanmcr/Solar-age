5c62c21c91ef2146eb973547d3e625ddef782e14
workmanmcr <workmanmcr@gmail.com>
2023-06-23 12:51:27 -0700

    refactor good fail

--- __tests__/solaragecalculator.test.js
+++ __tests__/solaragecalculator.test.js
@@ -3,17 +3,11 @@ import SolarAgeCalculator from "../src/js/solaragecalculator";
 describe("SolarAgeCalculator", () => {
 
     test("should create solar age claculator object that will calculate your age on different planets", () => {
-        const solarAgeCalculator = new SolarAgeCalculator(0, 0, 0, 0, 0);
-        expect(solarAgeCalculator.earthAge).toBe(0);
-        expect(solarAgeCalculator.mercuryAge).toBe(0);
-        expect(solarAgeCalculator.venusAge).toBe(0);
-        expect(solarAgeCalculator.marsAge).toBe(0);
-        expect(solarAgeCalculator.jupiterAge).toBe(0); 
+        const solarAgeCalculator = new SolarAgeCalculator(1);
+        expect(solarAgeCalculator.earthAge).toBe(1);
+        expect(solarAgeCalculator.mercuryAge).toBe(0.24);
+        expect(solarAgeCalculator.venusAge).toBe(0.62);
+        expect(solarAgeCalculator.marsAge).toBe(1.88);
+        expect(solarAgeCalculator.jupiterAge).toBe(11.86); 
     })
-
-     test("it should calculate your age on mercury based on your age on earth", () => {
-        const solarAgeCalculator = new SolarAgeCalculator();
-      solarAgeCalculator.getEarthAge();
-         expect(solarAgeCalculator.earthAge).toEqual(0);
- });
- });
+});
--- src/js/solaragecalculator.js
+++ src/js/solaragecalculator.js
@@ -1,12 +1,11 @@
 export default class SolarAgeCalculator {
-  constructor(earthAge,mercuryAge, venusAge, marsAge, jupiterAge) {
-    this.earthAge = earthAge;
-    this.mercuryAge = mercuryAge;
-    this.venusAge = venusAge;
-    this.marsAge = marsAge;
-    this.jupiterAge = jupiterAge;
+  constructor(baseEarthAge) {
+    this.earthAge = baseEarthAge;
+    this.mercuryAge = this.earthAge * 0.24;
+    this.venusAge = this.earthAge * 0.62;
+    this.marsAge = this.earthAge * 1.88;
+    this.jupiterAge = this.earthAge * 11.86;
   }
-  getEarthAge() {
-    
-  }
-}
+  
+
+};
