c9ce30e76b06e744c1b1595d6826de427f6e994f
workmanmcr <workmanmcr@gmail.com>
2023-06-23 11:56:29 -0700

    reset first good pass

--- __tests__/solaragecalculator.test.js
+++ __tests__/solaragecalculator.test.js
@@ -3,7 +3,19 @@ import SolarAgeCalculator from "../src/js/solaragecalculator";
 describe("SolarAgeCalculator", () => {
 
     test("should create solar age claculator object that will calculate your age on different planets", () => {
-        const solarAgeCalculator = new SolarAgeCalculator();
+        const solarAgeCalculator = new SolarAgeCalculator(0, 0, 0, 0, 0);
         expect(solarAgeCalculator.earthAge).toBe(0);
+        expect(solarAgeCalculator.mercuryAge).toBe(0);
+        expect(solarAgeCalculator.venusAge).toBe(0);
+        expect(solarAgeCalculator.marsAge).toBe(0);
+        expect(solarAgeCalculator.jupiterAge).toBe(0);
+      
+        
     })
-})
+
+//     test("it should calculate your age on mercury based on your age on earth", () => {
+//         const solarAgeCalculator = new SolarAgeCalculator();
+//         solarAgeCalculator.getMercuryAge();
+//         expect(solarAgeCalculator.()).toEqual(0);
+// });
+ });
--- src/js/solaragecalculator.js
+++ src/js/solaragecalculator.js
@@ -1,5 +1,10 @@
-export default function SolarAgeCalculator() {
-  constructor(); {
-    this.earthAge = 0;
+export default class SolarAgeCalculator {
+  constructor(earthAge,mercuryAge, venusAge, marsAge, jupiterAge) {
+    this.earthAge = earthAge;
+    this.mercuryAge = mercuryAge;
+    this.venusAge = venusAge;
+    this.marsAge = marsAge;
+    this.jupiterAge = jupiterAge;
   }
-};
+
+}
